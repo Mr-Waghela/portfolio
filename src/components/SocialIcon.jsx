8a562ae6a4ddd27e703c074d495a6029165b46e2
chirag.w <chirag.w@media.net>
2023-04-24 15:11:47 +0530

    updated github and download for resume

--- src/components/SocialIcon.jsx
+++ src/components/SocialIcon.jsx
@@ -1,8 +1,10 @@
 const SocialIcon = ({data}) => {
+  let isDownload = data.name == 'Resume' ? true : false; 
+  console.log(isDownload);
   return (
     <li>
-          <a href={data.url} target="_blank" rel="noopener" className="social-linkd"
-              dangerouslySetInnerHTML={{ __html: data.icon }}
+      <a href={data.url} target="_blank" rel="noopener" className="social-linkd"      download={isDownload}
+        dangerouslySetInnerHTML={{ __html: data.icon }}
             >
         </a>
     </li>
